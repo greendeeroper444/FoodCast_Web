d549c67e251b1a21b9e85f973390fc9770c1212a
GreendeeRoper444 <121622281+GreendeeRoper444@users.noreply.github.com>
2025-02-19 10:24:38 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import path from 'node:path'
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,17 +8,12 @@ export default defineConfig({
     react(), 
     svgr()
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Enables absolute imports
-    }
-  },
   build: {
     rollupOptions: {
       external: ['babel-runtime/regenerator']
     }
   },
   define: {
-    'process.env': {}, // Ensures compatibility for process.env usage
+    'process.env': {}, //this ensures compatibility if you're using process.env elsewhere
   },
 })
