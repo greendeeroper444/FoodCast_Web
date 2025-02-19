31e48c85a4bf5eb0762b19de31cafc7b044131da
GreendeeRoper444 <121622281+GreendeeRoper444@users.noreply.github.com>
2025-02-19 14:31:16 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,18 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      external: ['babel-runtime/regenerator']
+      external: ['babel-runtime/regenerator'],
+      input: {
+        main: 'index.html'
+      }
     }
   },
   define: {
     'process.env': {}, //this ensures compatibility if you're using process.env elsewhere
   },
+  server: {
+    fs: {
+      allow: ['public']
+    }
+  }
 })
