8bf98da1ce9fb0dee2aaf98a3cc568a0c107a58e
GreendeeRoper444 <121622281+GreendeeRoper444@users.noreply.github.com>
2025-02-20 10:58:36 +0800

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
+// console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
